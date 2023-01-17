961be0ff8c17c6443bbd79c930ada8eb6bee179f
Ilia <89165005679@ya.ru>
2023-01-17 18:54:43 +0400

    Added Task03

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -47,3 +47,37 @@ else
 {
     Console.WriteLine("Не правильный ввод!");
 }
+
+/*
+Алтернативный метод решения 
+
+Console.WriteLine("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine()) ;
+switch (number)
+{
+case 1:
+Console.WriteLine("Понедельник");
+break;
+case 2:
+Console.WriteLine("Вторник");
+break;
+case 3:
+Console.WriteLine("Среда");
+break;
+case 4:
+Console.WriteLine("Четверг");
+break;
+case 5:
+Console.WriteLine("Пятница");
+break;
+case 6:
+Console.WriteLine("Суббота");
+break;
+case 7:
+Console.WriteLine("Воскресенье");
+break;
+default:
+Console.WriteLine("Вы ввели не правильное число. Введите число от 1 до 7");
+break;
+}
+*/
