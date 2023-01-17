912140e1a0004fbd6820a708df3e3302647b2c13
Ilia <89165005679@ya.ru>
2023-01-17 18:42:00 +0400

    Решили задачу №3

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+/*
+Console.WriteLine("День недели:");
+Console.WriteLine("1 - Понедельник");
+Console.WriteLine("2 - Вторник");
+Console.WriteLine("3 - Среда");
+Console.WriteLine("4 - Четверг");
+Console.WriteLine("5 - Пятница");
+Console.WriteLine("6 - Суббота");
+Console.WriteLine("7 - Воскресенье");
+*/
+
+Console.Write("Введите число: ");
+string day = Console.ReadLine();
+if (day == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+else if (day == "2")
+{
+    Console.WriteLine("Вторник");
+}
+else if (day == "3")
+{
+    Console.WriteLine("Среда");
+}
+else if (day == "4")
+{
+    Console.WriteLine("Четверг");
+}
+else if (day == "5")
+{
+    Console.WriteLine("Пятница");
+}
+else if (day == "6")
+{
+    Console.WriteLine("Суббота");
+}
+else if (day == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Не правильный ввод!");
+}
