ddd84bf869f9ebe5032b55c56f7b17cd3000df54
Ilia <89165005679@ya.ru>
2023-01-17 18:05:15 +0400

    Решили задачу №1

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(number * number);
+
+
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 / number2 == number2)
+{
+    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+}
+else
+{
+   Console.WriteLine($"Число {number1} не является квадратом числа {number2}");  
+}
